93db9b43b8cd8e280a3df774d1691e23441d96cd
Bhavya Bansal <bhavyabansal0916@gmail.com>
2023-01-03 12:07:34 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
+const {connectDB} = require('./config/db');
 //middlewares
 app.use(express.json());
 app.use(express.static('content'));
-app.use()
-
+app.use(express.urlencoded({extended:false}));
 const PORT = 1338;
 app.listen(PORT,()=>{
     console.log("Server is running");
+    connectDB();
 })
